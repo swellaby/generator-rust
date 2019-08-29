645cdc6a6306ec387bf5625f3d1ed6dc5e89396e
calebcartwright <caleb.cartwright@outlook.com>
2019-08-29 13:59:08 -0500

    chore: version bumps

--- generators/app/interfaces/project-input.ts
+++ generators/app/interfaces/project-input.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import inquirer = require('inquirer');
+// import inquirer = require('inquirer');
 import yeoman = require('yeoman-generator');
 import IProjectConfig = require('./project-config');
 
@@ -11,7 +11,7 @@ interface IProjectInput {
     name: string;
     optionName: string;
     option?: yeoman.OptionConfig;
-    prompt: inquirer.Question;
+    prompt: yeoman.Question;
     /**
      * Attempts to apply the specified value to corresponding setting.
      * @param {any} value - The value provided..
